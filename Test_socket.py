67eca7efc6c975f49fe9c9ad54a3f9107b9e0290
Israel Ashush <israelashush@gmail.com>
2021-05-27 11:29:33 +0300

    init commit

---
+++ Test_socket.py
@@ -0,0 +1,22 @@
+import socket
+
+input_word = input("enter word: ")
+
+my_socket = socket.socket()
+my_socket.connect(("127.0.0.1",8820))
+
+my_socket.send(input_word.encode())
+data = ''
+while data == '':
+    data = my_socket.recv(1024).decode()
+print("The server send: {}".format(data))
+
+my_socket.close()
+
+
+def main():
+        print("i")
+
+
+if __name__ == '__main__':
+    main()
